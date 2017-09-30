81c8094c5ac2f71602285d873952856329292b68
Mohammad Haghighat <hosseinhg@hotmail.com>
2017-09-30 23:07:20 +0330

    Update patterns

--- pattern/pattern.py
+++ pattern/pattern.py
@@ -1,33 +1,18 @@
 import sys
 
 min_seq_length = 4
+Seq = list()
+length = [0] * 10
 
-def Middle(a, b):
-  """Return the node between a and b if there is one, or None."""
-  if (a+b)%2 != 0:
-    return None
-  mid = (a+b)/2
-  if mid == 5:
-    return mid
-  if a%3 == b%3:
-    return mid
-  if (a-1)/3 == (b-1)/3:
-    return mid
-  return None
 
-def NextValid(base):
-  """Generate valid moves j+1 given a sequence of moves 1..j."""
-  if len(base) >= 9:
-    return
-  if len(base) == 0:
-    for i in xrange(1,10):
-      yield i
-    return
-  for i in xrange(1,10):
-    if not i in base:
-      mid = Middle(i, base[-1])
-      if mid is None or mid in base:
-        yield i
+def middle(a, b):
+    """Return the node between a and b if there is one, or None."""
+    if (a + b) % 2 != 0:
+        return None
+    mid = (a + b) / 2
+    if mid == 5 or a % 3 == b % 3 or (a - 1) / 3 == (b - 1) / 3:
+        return mid
+
 
 def next(base):
     if len(base) == 0:
@@ -35,24 +20,27 @@ def next(base):
     tmp = list()
     for i in xrange(1, 10):
         if not i in base:
-            mid = Middle(i, base[-1])
+            mid = middle(i, base[-1])
             if mid is None or mid in base:
                 tmp.append(i)
     return tmp
 
-Seq = list()
-seqs_of_length = [0]*10
 
-def seq2(base):
+def generate(base):
     for n in next(base):
         s = base + [n]
-        seq2(s)
+        generate(s)
         Seq.append(s)
 
-seq2([])
-for seq in Seq:
-    if len(seq) >= min_seq_length:
-      seqs_of_length[len(seq)] += 1
 
-print 'Sequences by length:', seqs_of_length
-print 'Total number of sequences:', sum(seqs_of_length)
+def count():
+    generate([])
+    for seq in Seq:
+        if len(seq) >= min_seq_length:
+            length[len(seq)] += 1
+
+    print "length:", length, "total:", sum(length)
+
+
+if __name__ == "__main__":
+    count()
